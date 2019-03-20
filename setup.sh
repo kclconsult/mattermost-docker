7e8106b7cc4d91cc6953df426b5f7ac5f8e025de
Martin Chapman <contact@martinchapman.co.uk>
2019-03-20 23:32:12 +0000

    Config for single host docker-compose. Setup script. Proxy.

---
+++ setup.sh
@@ -0,0 +1,37 @@
+# docker-compose build
+# docker-compose up
+
+# Basic settings
+docker-compose exec app mattermost config set ServiceSettings.SiteURL "https://localhost/chat"
+docker-compose exec app mattermost config set TeamSettings.SiteName "CONSULT"
+docker-compose exec app mattermost config set TeamSettings.MaxNotificationsPerChannel 1
+docker-compose exec app mattermost config set TeamSettings.RestrictDirectMessage "team"
+docker-compose exec app mattermost config set EmailSettings.EnablePreviewModeBanner false
+docker-compose exec app mattermost config set ServiceSettings.EnablePostUsernameOverride true
+docker-compose exec app mattermost config set ServiceSettings.EnablePostIconOverride true
+docker-compose exec app mattermost config set ServiceSettings.AllowedUntrustedInternalConnections "localhost"
+docker-compose exec app mattermost config set PrivacySettings.ShowEmailAddress false
+docker-compose exec app mattermost config set PrivacySettings.ShowFullName false
+
+# docker-compose exec app mattermost config set ServiceSettings.EnableUserAccessTokens true
+
+# User and team creation
+docker-compose exec app mattermost user create --firstname connie --system_admin --email connie@consult.kcl.ac.uk --username connie --password 12345
+docker-compose exec app mattermost roles system_admin connie
+docker-compose exec app mattermost team create --name patient --display_name "Patient Chat"
+docker-compose exec app mattermost team add patient connie
+
+## Restrict after first team creation
+docker-compose exec app mattermost config set TeamSettings.MaxChannelsPerTeam 1
+
+## Sample user
+docker-compose exec app mattermost user create --firstname user --email user@consult.kcl.ac.uk --username user --password 12345
+docker-compose exec app mattermost team add patient user
+
+# Hooks
+docker-compose exec app mattermost webhook create-incoming --channel patient:off-topic --user connie --display-name connie
+docker-compose exec app mattermost command create patient --title start --description "start" --trigger-word start --url https://device-integration_nokia_proxy_1/dialogue/response --creator connie --response-username connie --autocomplete --post
+
+# Restart everything
+docker-compose down;
+# docker-compose up;
