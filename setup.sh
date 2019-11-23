c6196a6868771442a663784552393aca625ccd28
Martin Chapman <contact@martinchapman.co.uk>
2019-11-23 14:05:15 +0000

    Setup.sh: -- Don't explicitly create channel with hook and command in prod -- Reduce max users per team to 1 to limit communication

--- setup.sh
+++ setup.sh
@@ -7,20 +7,24 @@ fi
 
 sleep 5
 
-# User and team creation
-docker-compose exec app mattermost team create --name patient --display_name "Patient Chat"
-sleep 5
+# Admin
 docker-compose exec app mattermost user create --firstname connie --system_admin --email connie@consult.kcl.ac.uk --username connie --password 12345
-sleep 5
-docker-compose exec app mattermost team add patient connie
 sleep 5
 docker-compose exec app mattermost roles system_admin connie
 sleep 5
+
+if [ "$1" = "dev" ]; then
+  docker-compose exec app mattermost team create --name patient --display_name "Patient Chat"
+  sleep 5
+  docker-compose exec app mattermost team add patient connie
+  sleep 5
+fi
 
 # (dev) localhost for access to dialogue manager via port, (single host) docker-compose assigned address of single proxy or (multiple host) address of message-passer (attributed to trusted cert).
 docker-compose exec app mattermost config set ServiceSettings.AllowedUntrustedInternalConnections "localhost danvers"
 docker-compose exec app mattermost config set TeamSettings.SiteName "CONSULT"
 docker-compose exec app mattermost config set TeamSettings.MaxNotificationsPerChannel 1
+docker-compose exec app mattermost config set TeamSettings.MaxUsersPerTeam 1
 docker-compose exec app mattermost config set TeamSettings.RestrictDirectMessage "team"
 docker-compose exec app mattermost config set EmailSettings.EnablePreviewModeBanner false
 docker-compose exec app mattermost config set ServiceSettings.EnablePostUsernameOverride true
@@ -29,20 +33,17 @@ docker-compose exec app mattermost config set PrivacySettings.ShowEmailAddress f
 docker-compose exec app mattermost config set PrivacySettings.ShowFullName false
 sleep 5
 
-# Restrict after first team creation
-docker-compose exec app mattermost config set TeamSettings.MaxChannelsPerTeam 1
-docker-compose exec app mattermost config set TeamSettings.EnableTeamCreation false
-sleep 5
+if [ "$1" = "dev" ]; then
+  # Sample user
+  docker-compose exec app mattermost user create --firstname user --email user@consult.kcl.ac.uk --username user --password 12345
+  sleep 5
+  docker-compose exec app mattermost team add patient user
+  sleep 5
 
-# Sample user
-docker-compose exec app mattermost user create --firstname user --email user@consult.kcl.ac.uk --username user --password 12345
-sleep 5
-docker-compose exec app mattermost team add patient user
-sleep 5
-
-# Hooks
-docker-compose exec app mattermost webhook create-incoming --channel patient:off-topic --user connie --display-name connie
-sleep 5
+  # Hooks
+  docker-compose exec app mattermost webhook create-incoming --channel patient:off-topic --user connie --display-name connie
+  sleep 5
+fi
 
 # Supply localhost to reference single proxy if in dev, otherwise reference allocated proxy hostname under single docker machine (host) (e.g. 'device-integration_nokia_proxy_1') or machine address (attributed to added trusted cert., e.g. 'danvers') if using multiple docker machines (host). Avoid hard-coding ports to enable possible service discovery.
 if [ "$1" = "dev" ]; then
@@ -50,9 +51,6 @@ if [ "$1" = "dev" ]; then
   sleep 5
 elif [ "$1" = "demo" ]; then
   docker-compose exec app mattermost command create patient --title start --description "start" --trigger-word start --url https://danvers:3005/dialogue/response --creator connie --response-username connie --autocomplete --post
-  sleep 5
-else
-  docker-compose exec app mattermost command create patient --title start --description "start" --trigger-word start --url https://danvers/dialogue/response --creator connie --response-username connie --autocomplete --post
   sleep 5
 fi
 
